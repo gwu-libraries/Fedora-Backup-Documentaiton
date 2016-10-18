a0a41e92aa04a40ba7e9cc37b15392c10dd5e5ba
Matt Mihalik <matt.mihalik@gmail.com>
2016-10-18 09:19:23 -0400

    Organize logs into year and month folders

--- scripts/repositoryFixity.sh
+++ scripts/repositoryFixity.sh
@@ -1,7 +1,9 @@
 #/bin/bash
 
-# Set date of yesterdays logs
-YESTERDAY=$(date +"%Y%m%d" --date="1 day ago") 
+# Set dates
+YESTERDAY=$(date +"%Y%m%d" --date="1 day ago")
+YEAR=$(date +"%Y" --date="1 day ago")
+MONTH=$(date +"%m" --date="1 day ago")
 # Set users for email alerts
 EMAIL=""
 # Configure GitHub credentials
@@ -10,22 +12,43 @@ GITHUB_CREDENTIALS=""
 #Set the server URL
 SERVER_URL=""
 
+# Create folders for year and month if they don't exist already
+if [ ! -d /opt/Fedora-Backup-Documentation/fixitySuccesses/$YEAR ]
+	then
+		mkdir /opt/Fedora-Backup-Documentation/fixitySuccesses/$YEAR
+fi
+
+if [ ! -d /opt/Fedora-Backup-Documentation/fixitySuccesses/$YEAR/$MONTH ]
+	then
+		mkdir /opt/Fedora-Backup-Documentation/fixitySuccesses/$YEAR/$MONTH
+fi
+
+if [ ! -d /opt/Fedora-Backup-Documentation/fixityErrors/$YEAR ]
+	then
+		mkdir /opt/Fedora-Backup-Documentation/fixityErrors/$YEAR
+fi
+
+if [ ! -d /opt/Fedora-Backup-Documentation/fixityErrors/$YEAR/$MONTH ]
+	then
+		mkdir /opt/Fedora-Backup-Documentation/fixityErrors/$YEAR/$MONTH
+fi
+
 # Record items that passed the fixity check
 if [ -f /tmp/fixitySuccess.log ]
 	then
-		cat /tmp/fixitySuccess.log | grep -e "<rdf:Description rdf:about=" -e "sha1" | grep -v fixity >> /opt/Fedora-Backup-Documentation/fixitySuccesses/$YESTERDAY-fixitySuccesses.log
+		cat /tmp/fixitySuccess.log | grep -e "<rdf:Description rdf:about=" -e "sha1" | grep -v fixity >> /opt/Fedora-Backup-Documentation/fixitySuccesses/$YEAR/$MONTH/$YESTERDAY-fixitySuccesses.log
 		cd /opt/Fedora-Backup-Documentation/
 		git pull
-		git add /opt/Fedora-Backup-Documentation/fixitySuccesses/$YESTERDAY-fixitySuccesses.log
-		SUCCESSES=$(grep -e "<rdf:Description rdf:about=" /opt/Fedora-Backup-Documentation/fixitySuccesses/$YESTERDAY-fixitySuccesses.log | wc -l)
+		git add /opt/Fedora-Backup-Documentation/fixitySuccesses/$YEAR/$MONTH/$YESTERDAY-fixitySuccesses.log
+		SUCCESSES=$(grep -e "<rdf:Description rdf:about=" /opt/Fedora-Backup-Documentation/fixitySuccesses/$YEAR/$MONTH/$YESTERDAY-fixitySuccesses.log | wc -l)
 	else
 		# No items passed the fixity check
 		echo "Problems!"
-		touch /opt/Fedora-Backup-Documentation/fixitySuccesses/$YESTERDAY-fixitySuccesses.log
+		touch /opt/Fedora-Backup-Documentation/fixitySuccesses/$YEAR/$MONTH/$YESTERDAY-fixitySuccesses.log
 		cd /opt/Fedora-Backup-Documentation/
 		git pull
-		git add /opt/Fedora-Backup-Documentation/fixitySuccesses/$YESTERDAY-fixitySuccesses.log
-		SUCCESSES=$(grep -e "<rdf:Description rdf:about=" /opt/Fedora-Backup-Documentation/fixitySuccesses/$YESTERDAY-fixitySuccesses.log | wc -l)
+		git add /opt/Fedora-Backup-Documentation/fixitySuccesses/$YEAR/$MONTH/$YESTERDAY-fixitySuccesses.log
+		SUCCESSES=$(grep -e "<rdf:Description rdf:about=" /opt/Fedora-Backup-Documentation/fixitySuccesses/$YEAR/$MONTH/$YESTERDAY-fixitySuccesses.log | wc -l)
 fi
 
 # Push successful fixity results to Github repo
@@ -36,18 +59,18 @@ git push $GITHUB_CREDENTIALS
 # Record items that failed the fixity check
 if [ -f /tmp/fixityErrors.log ]
 	then
-		cat /tmp/fixityErrors.log | grep -e "<rdf:Description rdf:about=" -e "sha1" | grep -v fixity >> /opt/Fedora-Backup-Documentation/fixityErrors/$YESTERDAY-fixityErrors.log
+		cat /tmp/fixityErrors.log | grep -e "<rdf:Description rdf:about=" -e "sha1" | grep -v fixity >> /opt/Fedora-Backup-Documentation/fixityErrors/$YEAR/$MONTH/$YESTERDAY-fixityErrors.log
 		cd /opt/Fedora-Backup-Documentation/
-		git add /opt/Fedora-Backup-Documentation/fixityErrors/$YESTERDAY-fixityErrors.log
-		ERRORS=$(grep -e "<rdf:Description rdf:about=" /opt/Fedora-Backup-Documentation/fixityErrors/$YESTERDAY-fixityErrors.log | wc -l)
+		git add /opt/Fedora-Backup-Documentation/fixityErrors/$YEAR/$MONTH/$YESTERDAY-fixityErrors.log
+		ERRORS=$(grep -e "<rdf:Description rdf:about=" /opt/Fedora-Backup-Documentation/fixityErrors/$YEAR/$MONTH/$YESTERDAY-fixityErrors.log | wc -l)
 	else
 		# Some items failed the fixity check
 		echo "No errors!"
-		touch /opt/Fedora-Backup-Documentation/fixityErrors/$YESTERDAY-fixityErrors.log
+		touch /opt/Fedora-Backup-Documentation/fixityErrors/$YEAR/$MONTH/$YESTERDAY-fixityErrors.log
 		cd /opt/Fedora-Backup-Documentation/
-		git add /opt/Fedora-Backup-Documentation/fixityErrors/$YESTERDAY-fixityErrors.log
-		ERRORS=$(grep -e "<rdf:Description rdf:about=" /opt/Fedora-Backup-Documentation/fixityErrors/$YESTERDAY-fixityErrors.log | wc -l)
-		echo "Fixity checks on $SERVER_URL failed for $ERRORS items.  See: https://github.com/gwu-libraries/Fedora-Backup-Documentation/tree/master/fixityErrors/$YESTERDAY-fixityErrors.log for more details" | mail -s "Fixity checks failed for $ERRORS items" $EMAILS
+		git add /opt/Fedora-Backup-Documentation/fixityErrors/$YEAR/$MONTH/$YESTERDAY-fixityErrors.log
+		ERRORS=$(grep -e "<rdf:Description rdf:about=" /opt/Fedora-Backup-Documentation/fixityErrors/$YEAR/$MONTH/$YESTERDAY-fixityErrors.log | wc -l)
+		echo "Fixity checks on $SERVER_URL failed for $ERRORS items.  See: https://github.com/gwu-libraries/Fedora-Backup-Documentation/tree/master/fixityErrors/$YEAR/$MONTH/$YESTERDAY-fixityErrors.log for more details" | mail -s "Fixity checks failed for $ERRORS items" $EMAILS
 fi
 
 # Push fixity error results to Github repo
